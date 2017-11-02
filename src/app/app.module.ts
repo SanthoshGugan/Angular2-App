c8d6dccc46e058dd8dd35901c9da2115eb0bdec9
Santhosh Gugan <santhoshg1990@gmail.com>
2017-11-02 23:22:55 +0530

    box office component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoxOfficeComponent } from './box-office/box-office.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxOfficeComponent
   ],
   imports: [
     BrowserModule
